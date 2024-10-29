5d0d479e81a995d1434262cf5ae4091ef382e9a3
zlj0402 <2092416525@qq.com>
2024-10-29 23:11:17 +0800

    Binary Search Tree - version 1: recursion

--- BST/src/BST.java
+++ BST/src/BST.java
@@ -4,13 +4,13 @@ public class BST<E extends Comparable<E>> {
         public E e;
         public Node left, right;
 
-        public Node() {
-            e = null;
+        public Node(E e) {
+            this.e = e;
             left = right = null;
         }
     }
 
-    public BST root;
+    public Node root;
     public int size;
 
     public BST(){
@@ -27,6 +27,36 @@ public class BST<E extends Comparable<E>> {
     }
 
     public void add(E e) {
+        if (root == null) {
+            root = new Node(e);
+            size++;
+        }
+        else {
+            add(root, e);
+        }
+    }
+
+    private void add(Node node, E e) {
+        if (e.equals(node.e)) {
+            return;
+        }
+        else if (e.compareTo(node.e) < 0 && node.left == null) {
+            node.left = new Node(e);
+            size++;
+            return;
+        }
+        else if(e.compareTo(node.e) > 0 && node.right == null) {
+            node.right = new Node(e);
+            size++;
+            return;
+        }
+
+        if (e.compareTo(node.e) < 0) {
+            add(node.left, e);
+        }
+        else{
+            add(node.right, e);
+        }
 
     }
 }
